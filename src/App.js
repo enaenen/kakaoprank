486573c99a6e824a479240a1072f25e9dced3c02
space <enaenen@naver.com>
2020-06-03 21:21:55 +0900

    readme modify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 function App() {
-    return ( <
-        div className = "App" >
-
-        <
-        /div>
+    return ( <div className = "App" >
+        </div>
     );
 }
 
