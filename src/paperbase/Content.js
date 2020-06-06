38c5d435a21b7d786e9987f625c8fdc7fe59763e
SpaceChae <enaenen@naver.com>
2020-06-06 15:24:31 +0900

    search and refresh icon delete

--- src/paperbase/Content.js
+++ src/paperbase/Content.js
@@ -7,11 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import { withStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import RefreshIcon from '@material-ui/icons/Refresh';
 
 const styles = (theme) => ({
   paper: {
@@ -44,13 +40,10 @@ function Content(props) {
       <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
         <Toolbar>
           <Grid container spacing={2} alignItems="center">
-            <Grid item>
-              <SearchIcon className={classes.block} color="inherit" />
-            </Grid>
             <Grid item xs>
               <TextField
                 fullWidth
-                placeholder="Search by email address, phone number, or user UID"
+                placeholder="금액을 입력하세요."
                 InputProps={{
                   disableUnderline: true,
                   className: classes.searchInput,
@@ -59,13 +52,8 @@ function Content(props) {
             </Grid>
             <Grid item>
               <Button variant="contained" color="primary" className={classes.addUser}>
-                Add user
+                전송하기
               </Button>
-              <Tooltip title="Reload">
-                <IconButton>
-                  <RefreshIcon className={classes.block} color="inherit" />
-                </IconButton>
-              </Tooltip>
             </Grid>
           </Grid>
         </Toolbar>
