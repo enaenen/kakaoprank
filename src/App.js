c2490cf4cd39625baf7646fcd37bf80fbd33805a
LouiMinister <LouiMinister@gmail.com>
2020-06-12 23:04:23 +0900

    Add Kakao SDK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Paperbase from './Paperbase';
 
 const App = ()=>{
+    useEffect(() => {
+        console.log("useEffect")
+        const script = document.createElement("script");
+        //script.async = true;
+        script.src ="//developers.kakao.com/sdk/js/kakao.min.js";
+        document.body.appendChild(script);
+      },[]);
+
+
     return (
         <div className="App">
             <Paperbase></Paperbase>
