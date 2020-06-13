3f34d65db145a6b7f36f7d8004f25c46c7ec7b5d
space <enaenen@naver.com>
2020-06-14 03:33:07 +0900

    screen active

--- src/components/innerContent/TransferMoney.js
+++ src/components/innerContent/TransferMoney.js
@@ -74,12 +74,12 @@ function TransferMoney(props) {
   return (
       <>
         <div className={classes.leftArea}>
-            <img src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b03b13c3-b39e-4fcb-9c54-406fb04e2826/fake.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200606%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200606T083251Z&X-Amz-Expires=86400&X-Amz-Signature=8958b83114b069fe5a1ac8fdc32963a29fe6bec2a480f75b99cc3e2be959125f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22fake.jpg%22'
+            <img src='https://i.ibb.co/LgGBqDj/money.png'
             className={classes.img} alt="x"></img>
         </div>
         <div className={classes.rightArea}>
             <form className={classes.btn} noValidate autoComplete="off">
-            <TextField id="outlined-basicc" label="금액" variant="outlined" />
+            <TextField id="outlined-basicc" label="금액" variant="outlined"/>
             </form>
         </div>
         <button onClick = {()=>console.log(window.Kakao)}>임시</button>
