9a6d6b198d549960d9f8f7ba3b7c59da2ca346b5
SpaceChae <enaenen@naver.com>
2020-06-17 18:28:09 +0900

    construction

--- src/components/innerContent/SendGifticon.js
+++ src/components/innerContent/SendGifticon.js
@@ -39,24 +39,26 @@ const styles = (theme) => ({
   }
 
 });
-
+//https://i.ibb.co/mR5ghm2/img1-daumcdn.jpg
 function SendGifticon(props) {
   const { classes } = props;
 
   return (
       <div>
         <div className={classes.leftArea}>
-            <img src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b03b13c3-b39e-4fcb-9c54-406fb04e2826/fake.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200606%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200606T083251Z&X-Amz-Expires=86400&X-Amz-Signature=8958b83114b069fe5a1ac8fdc32963a29fe6bec2a480f75b99cc3e2be959125f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22fake.jpg%22'
+            <img src='https://i.ibb.co/mR5ghm2/img1-daumcdn.jpg'
             className={classes.img} alt="x"></img>
         </div>
         <div className={classes.rightArea}>
-            <form className={classes.btn} noValidate autoComplete="off">
+
+          <h2>준비중이라 미안합니다.</h2>
+            {/* <form className={classes.btn} noValidate autoComplete="off">
             기프티콘 보내기
             <TextField id="outlined-basicc" label="금액" variant="outlined" />
             <Button startIcon={<ShareIcon/>} variant="contained" color="primary" id="kakao-link-btn" className={classes.share}>
                  공유하기
               </Button>
-            </form>
+            </form> */}
         </div>
     </div>
   );
