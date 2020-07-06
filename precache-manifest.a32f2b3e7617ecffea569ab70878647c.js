bd369822d7b208d16b5fd8a4f9b79ea27729a6c5
SpaceChae <enaenen@naver.com>
2020-07-06 14:47:39 +0900

    Updates

---
+++ precache-manifest.a32f2b3e7617ecffea569ab70878647c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c12c3794a106d19e9284fd138a18356",
+    "url": "/kakaoprank/index.html"
+  },
+  {
+    "revision": "b1c9beb79ce5075ea2bb",
+    "url": "/kakaoprank/static/js/2.d5208633.chunk.js"
+  },
+  {
+    "revision": "6ead315f60788879c491a87054985612",
+    "url": "/kakaoprank/static/js/2.d5208633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10536d71642e1b4d4ac",
+    "url": "/kakaoprank/static/js/main.81e9815c.chunk.js"
+  },
+  {
+    "revision": "2a908d6f6fd138b97ed6",
+    "url": "/kakaoprank/static/js/runtime-main.93c61575.js"
+  }
+]);
