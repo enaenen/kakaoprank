1942e2059775e0dfabb7a706bb5a1d5ee6e55f81
SpaceChae <enaenen@naver.com>
2020-07-07 15:38:19 +0900

    link fix

--- src/Paperbase.js
+++ src/Paperbase.js
@@ -11,7 +11,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://github.com/enaenen/kakaoprank">
         LouiMinister & Space 's React ToyProject
       </Link>{' '}
       {new Date().getFullYear()}
